05a94ec9172fef73cacf7bf4a58a124c7cf8f325
Giuseppe <dimontegiuseppe.1@gmail.com>
2024-04-10 12:05:11 +0200

    Feature personalised message appear depending on the score

---
+++ assets/js/end.js
@@ -0,0 +1,26 @@
+const finalScore = document.getElementById('final-score-end');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+finalScore.innerText = mostRecentScore;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const finalScore = document.getElementById('final-score-end');
+    const mostRecentScore = localStorage.getItem('mostRecentScore');
+    finalScore.innerText = mostRecentScore;
+
+    let personalisedMessage = '';
+    if (mostRecentScore >= 8) {
+        personalisedMessage = "Great job! You scored " + mostRecentScore + ". Can you beat it next time?";
+    } else if (mostRecentScore >= 5) {
+        personalisedMessage = "Well done! You scored " + mostRecentScore + ". Keep practicing to improve!";
+    } else {
+        personalisedMessage = "Nice try! You scored " + mostRecentScore + ". Practice more to get better results!";
+    }
+
+    const messageElement = document.createElement('p');
+    messageElement.id = 'personalised-message';
+    messageElement.classList.add('scale-in');
+    messageElement.textContent = personalisedMessage;
+
+    const endPage = document.getElementById('end-page');
+    endPage.appendChild(messageElement);
+});
